d31dcffac276642a8809bbdb576ff6a025cc2a92
Alexander Warth <43041463+CochainComplex@users.noreply.github.com>
2025-10-05 15:57:54 -0400

    more scribble

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,16 @@ def emotion_detection(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } } 
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
-    return response.text
+
+    formatted_response = json.loads(response.text)
+
+    label = None
+    score = None
+
+    #print(formatted_response)
+    
+    label = formatted_response['emotionPredictions']['emotion']
+    print(label)
+    #score = formatted_response['documentSentiment']['score']
+
+    return label
