8c5211df53d3f1ea89ec5049e3547e4147d0d7da
Alexander Warth <43041463+CochainComplex@users.noreply.github.com>
2025-10-12 06:55:32 -0400

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,14 +8,30 @@ def emotion_detection(text_to_analyse):
     response = requests.post(url, json=myobj, headers=header)
 
     formatted_response = json.loads(response.text)
-
-    label = None
-    score = None
-
-    #print(formatted_response)
     
-    label = formatted_response['emotionPredictions']['emotion']
-    print(label)
-    #score = formatted_response['documentSentiment']['score']
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
 
-    return label
+    anger_score = emotions['anger']
+    distgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness'] 
+
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': distgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        'anger': anger_score,
+        'disgust': distgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
